09c6353a58147f54179135159507a8b034e4e765
gloz0315 <qudtn0315@naver.com>
2024-01-20 17:35:27 +0900

    Application : JpaAuditing을 사용하기 위해 @EnableJpaAuditing 어노테이션 추가

--- src/main/java/simpleprojects/scheduleproject/ScheduleProjectApplication.java
+++ src/main/java/simpleprojects/scheduleproject/ScheduleProjectApplication.java
@@ -2,7 +2,9 @@ package simpleprojects.scheduleproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing      // JpaAuditing 을 사용한다고 정보를 전달해준다 -> 꼭 달아야지 timestamp 가능
 @SpringBootApplication
 public class ScheduleProjectApplication {
 
