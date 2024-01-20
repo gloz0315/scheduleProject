632f39b71c70795080b13ef451a344d8a4cb3ddb
gloz0315 <qudtn0315@naver.com>
2024-01-20 18:30:20 +0900

    HTTP 상태 코드와 메세지에 대한 기능 생성

---
+++ src/main/java/simpleprojects/scheduleproject/status/HttpStatus.java
@@ -0,0 +1,9 @@
+package simpleprojects.scheduleproject.status;
+
+public class HttpStatus {
+    public static final int HTTP_STATUS_OK = 200;
+    public static final int HTTP_STATUS_CREATE = 201;
+    public static final int HTTP_STATUS_BAD_REQUEST = 400;
+    public static final int HTTP_STATUS_NOT_FOUND = 404;
+
+}
