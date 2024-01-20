42ebc39bca6cd9e569cb929a12cefe0ad8813418
gloz0315 <qudtn0315@naver.com>
2024-01-20 17:33:42 +0900

    RequestDto 클래스 생성 (제목,내용,작성자, 비밀번호) 기능 추가

---
+++ src/main/java/simpleprojects/scheduleproject/dto/RequestDto.java
@@ -0,0 +1,12 @@
+package simpleprojects.scheduleproject.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RequestDto {
+    private String title;
+    private String contents;
+    private String author;
+    private String password;
+
+}
